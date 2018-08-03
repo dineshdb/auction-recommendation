66879966621ca572ffcf46108b06546ba8ee7499
Jeevan Thapa <jeevanthapa9111@gmail.com>
2018-08-04 00:49:25 +0545

    get values from latest model

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import numpy as np
 from flask import *
 from flask_cors import CORS
+import os
+import glob
 
 app = Flask(__name__)
 CORS(app)
@@ -8,9 +10,11 @@ CORS(app)
 
 @app.route('/<int:user_idx>')
 def get(user_idx):
-    Ratings = np.load('model/ratings.npy')
-    row_factor = np.load('model/row.npy')
-    col_factor = np.load('model/col.npy')
+    modelList = glob.glob('model/*')
+    latestFolder = max(modelList, key=os.path.getctime)
+    Ratings = np.load(latestFolder + '/ratings.npy')
+    row_factor = np.load(latestFolder + '/row.npy')
+    col_factor = np.load(latestFolder + '/col.npy')
     user_rated = [i[1] for i in Ratings if i[0] == user_idx]
 
     assert (row_factor.shape[0] - len(user_rated)) >= 5
